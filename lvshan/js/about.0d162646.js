af4b3655aaff4847bef67de17f0893a10d580851
lanka <rev1val@qq.com>
2020-05-31 00:42:41 +0800

    update lvshan

---
+++ lvshan/js/about.0d162646.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["about"],{f820:function(t,n,e){"use strict";e.r(n);var s=e("2877"),a=Object(s.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"about"})}),[],!1,null,null,null);n.default=a.exports}}]);
---
+++ lvshan/precache-manifest.023b4e6038bc8ca855ad80961d43d1ca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e877667ed15b6faec3",
+    "url": "/css/app.af58bcec.css"
+  },
+  {
+    "revision": "095d2136607b968355b4",
+    "url": "/css/vendor.1bc8cbb4.css"
+  },
+  {
+    "revision": "143146fa24554ae2c5ac0a3982abb952",
+    "url": "/fonts/ionicons.143146fa.woff2"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "b1eb7ab518f18c9287bc915ecb7465ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2696751fa02070c463ad",
+    "url": "/js/about.0d162646.js"
+  },
+  {
+    "revision": "b8e877667ed15b6faec3",
+    "url": "/js/app.98090622.js"
+  },
+  {
+    "revision": "095d2136607b968355b4",
+    "url": "/js/vendor.b647fefe.js"
+  },
+  {
+    "revision": "b13b5dbddd0ed374d4a530c551cecf06",
+    "url": "/manifest.json"
+  }
+]);
